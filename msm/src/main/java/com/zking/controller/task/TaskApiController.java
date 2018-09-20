78d4839d0e261f40eb370aa664c6a1715a22f952
Jerry <JERRY1ZHANG>
2018-09-20 15:40:00 +0800

    清空文件

--- msm/src/main/java/com/zking/controller/task/TaskApiController.java
+++
@@ -1,9 +0,0 @@
-package com.zking.controller.task;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/apitask")
-public class TaskApiController {
-}
--- msm/src/main/java/com/zking/service/ListStaticService.java
+++
@@ -1,9 +0,0 @@
-package com.zking.service;
-
-import com.zking.pojo.ListStatic;
-
-import java.util.List;
-
-public interface ListStaticService {
-    List<ListStatic> getList(String list_type);
-}
